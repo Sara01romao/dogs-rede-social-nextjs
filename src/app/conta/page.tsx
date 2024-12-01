8dee924841453b62dddc32587cf425f3caad5f87
Sara Romao <sara-romao@live.com>
2024-12-01 18:14:11 -0300

    feat: context api usuario

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+import { useUser } from "@/context/user-context"
 
 export default async function ContaPage() {
-
+   const {user} = useUser();
     return (
         <main>
-            <h1>Conta</h1>
+            <h1>Conta: {user?.nome}</h1>
         </main>
     )
     
