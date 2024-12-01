5de013c6c8975c7615c2cad02078b9cec5af1e73
Sara Romao <sara-romao@live.com>
2024-12-01 10:50:53 -0300

    feat: get tonek user logado

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import styles from "./header.module.css"
 import Image from "next/image";
+import userGet from "@/actions/user-get";
 
 export default async function Header() {
-    const user = true;
-
+    const {data} = await userGet();
+ 
     return (
         <header className={styles.header}>
             <nav className={`${styles.nav} container`}>
@@ -18,9 +19,9 @@ export default async function Header() {
                     />
                 </Link>
 
-                {user? (
+                {data? (
                     <Link className={styles.login} href={'/conta'}>
-                        dogs
+                        {data.nome}
 
                     </Link>
                 ):(
