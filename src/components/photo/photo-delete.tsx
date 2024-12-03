1bd185770150491ff562794713bf10e64f72d7aa
Sara Romao <sara-romao@live.com>
2024-12-03 19:18:12 -0300

    feat: deletar foto

--- src/components/photo/photo-delete.tsx
+++ src/components/photo/photo-delete.tsx
@@ -1,4 +1,33 @@
+'use client';
 
-export default async function PhotoDelete({id}: {id:string}) {
-    return <button>Deletar: {id}</button>
+import { useState } from "react";
+import styles from './photo-delete.module.css';
+import photoDelete from "@/actions/photo-delete";
+
+export default function PhotoDelete({id}: {id:string}) {
+    const [loading, setLoading] = useState(false);
+
+    async function handleClick() {
+        setLoading(true);
+
+        const confirm = window.confirm('Tem  certeza que deseja deletar?');
+
+        if(confirm){
+           await photoDelete(id)
+        }
+
+        setLoading(false);
+        
+    }
+
+    return (
+        <>
+         {loading ?(
+            <button disabled className={styles.delete}>Deletar</button>
+         ):(
+            <button onClick={handleClick} className={styles.delete}>Deletar</button>
+         )}
+        
+        </>
+    ) 
 }
