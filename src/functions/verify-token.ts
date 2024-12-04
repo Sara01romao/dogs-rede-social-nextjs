ccef37f00e7015740b70ac13b441e95db40042db
Sara Romao <sara-romao@live.com>
2024-12-04 13:49:29 -0300

    feat: jwt verificação

---
+++ src/functions/verify-token.ts
@@ -0,0 +1,17 @@
+// import { jwtVerify } from "jose";
+
+export default async function verifyToken(token:string):Promise<boolean> {
+
+    if(!token) return false;
+
+    try{
+        // jwtVerify(token, new TextEncoder().encode(''), {
+        //     algorithms:['HS256'],
+        // })
+
+        return true;
+    }catch (error){
+        return false;
+    }
+    
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 
 import { NextRequest, NextResponse } from "next/server";
+import verifyToken from "./functions/verify-token";
 
 export async function middleware(request:NextRequest){
 
     const token = request.cookies.get('token')?.value;
-    const authenticated = token ? true : false;
+    const authenticated = token ? await verifyToken(token) : false;
 
     if(!authenticated && request.nextUrl.pathname.startsWith('/conta')){
         return NextResponse.redirect(new URL('/login', request.url));
